e25d8bf3d07d5ff51e860faa881ea200f78c16c6
Noah Gauci <breakf117@gmail.com>
2025-08-10 18:34:09 +1000

    changed website name and icon

--- frontend/src/components/BackgroundParticles.jsx
+++ frontend/src/components/BackgroundParticles.jsx
@@ -1,25 +1,82 @@
+import React from "react";
 import Particles from "react-tsparticles";
 
-function BackgroundParticles() {
+const BackgroundParticles = () => {
   return (
     <Particles
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100vw",
+        height: "100vh",
+        zIndex: 0,
+        pointerEvents: "none",
+      }}
       options={{
+        background: {
+          color: {
+            value: "rgb(255, 137, 235)", // pinkish background
+          },
+        },
+        fpsLimit: 60,
+        interactivity: {
+          detectsOn: "canvas",
+          events: {
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            repulse: {
+              distance: 100,
+              duration: 0.4,
+            },
+          },
+        },
         particles: {
-          number: { value: 30 },
-          shape: {
-            type: "heart"
+          color: {
+            value: "#ff66cc",
           },
-          size: { value: 10 },
           move: {
-            direction: "top",
-            speed: 2,
-            outMode: "out"
+            direction: "none",
+            enable: true,
+            outMode: "bounce",
+            random: true,
+            speed: 1,
+            straight: false,
           },
-          opacity: { value: 0.7 }
-        }
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 30,
+          },
+          opacity: {
+            value: 0.7,
+          },
+          shape: {
+            type: "char",
+            character: {
+              value: ["❤", "💕", "❣️", "💖"],
+              font: "Arial",
+              style: "",
+              weight: "400",
+              fill: true,
+            },
+          },
+          size: {
+            value: 20,
+            random: { enable: true, minimumValue: 10 },
+          },
+        },
+        detectRetina: true,
       }}
     />
   );
-}
+};
 
 export default BackgroundParticles;
