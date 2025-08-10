8760119c612c7f8a1b7146d92e2a42733343da7d
Noah Gauci <breakf117@gmail.com>
2025-08-11 02:47:25 +1000

    made spinner in centre of screen

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
 const Loader = () => (
-  <div className="flex justify-center items-center h-32">
-    <div className="w-12 h-12 border-4 border-pink-300 border-t-pink-500 rounded-full animate-spin"></div>
+  <div className="fixed inset-0 flex items-center justify-center bg-pink-50 z-50">
+    <div className="w-16 h-16 border-4 border-pink-200 border-t-pink-500 rounded-full animate-spin"></div>
   </div>
 );
 
